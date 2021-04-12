7f108f5f134f7edc7020b9acd06c4089b658ab28
JhonnyExtreme <jhony.extrem2305@gmail.com>
2021-04-12 03:18:53 -0300

    estatico

---
+++ pages/tempo.js
@@ -0,0 +1,29 @@
+
+
+function Tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString= dynamicDate.toGMTString();
+    return(
+        <center>
+            <div>
+                {dynamicDateString}(dinâmico)
+            </div>
+            <div>
+                {props.staticDateString}(Estático)
+            </div>
+        </center>
+    );
+
+}
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString= staticDate.toGMTString();
+    return{
+        props:{
+            staticDateString
+        }
+    }
+}
+
+export default Tempo
